bc0d2e533a24d12757bd437121637e2c074108f4
Simone Carletti <weppos@weppos.net>
2015-03-26 15:19:18 +0100

    Let's Dig into something cool

--- app.go
+++ app.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello Simone!")
+	var out string
+	var err error
+
+	out, err = Dig("example.com")
+	if err != nil {
+		log.Fatalf("Execution failed: %s", err)
+	} else {
+		log.Println("Executed %s", out)
+	}
+}
+
+func Dig(arg string) (string, error) {
+	if (len(arg) < 5) {
+		return "", fmt.Errorf("arg is too short")
+	}
+
+	return fmt.Sprintf("dig %s", arg), nil
 }
